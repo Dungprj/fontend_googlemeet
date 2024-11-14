40fe61f18b884bdc5a527dd1666b3d5ce1fedd3f
Dungprj <doibatcong2003@gmail.com>
2024-11-14 16:15:12 +0700

    bai96

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from "classnames/bind"
 import styles from "./Header.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark,faMagnifyingGlass,faSpinner } from "@fortawesome/free-solid-svg-icons"
+
+import logoTikTok from '~/assets/images/logotiktok.jpg';
 
 
 
@@ -7,7 +11,25 @@ const cx = classNames.bind(styles)
 
 function Header() {
   return <header className={cx("wrapper")}>
-    <div className={cx("inner")}></div>
+    <div className={cx("inner")}>
+    <img src={logoTikTok} alt="Tiktok" width={150}></img>
+      <div className={cx("search")}>
+        <input placeholder="Search Account and videos" spellCheck={false} />
+        <button className={cx("clear")}>
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </button>
+        <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+        
+        <button className={cx("search-btn")}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+
+        </button>
+      </div>
+
+      <div className={cx("actions")}>
+
+      </div>
+    </div>
   </header>
 }
 
